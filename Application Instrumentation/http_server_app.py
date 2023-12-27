275803dacb727b89edc62a5e66ee7be649407e48
Tola Awonusi <tola.awonusi@yahoo.com>
2023-12-27 15:01:05 +0000

    I modified the ip address to any ip address

--- Application Instrumentation/http_server_app.py
+++ Application Instrumentation/http_server_app.py
@@ -12,6 +12,6 @@ class HandleRequests(http.server.BaseHTTPRequestHandler):
 
 if __name__ == "__main__":
     start_http_server(5001)
-    server = http.server.HTTPServer(('13.40.117.12', 5000), HandleRequests)
+    server = http.server.HTTPServer(('0.0.0.0', 5000), HandleRequests)
     #server = http.server.HTTPServer(('localhost', 5000), HandleRequests)
     server.serve_forever()
