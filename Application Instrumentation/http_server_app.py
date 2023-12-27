57fa3fcd692cfbdd3c5d4d595fc53c05d72140d2
Tola Awonusi <tola.awonusi@yahoo.com>
2023-12-27 12:51:52 +0000

    Modified the ipv4 address in the http_server_app.py file to be that of AWS.

--- Application Instrumentation/http_server_app.py
+++ Application Instrumentation/http_server_app.py
@@ -12,6 +12,6 @@ class HandleRequests(http.server.BaseHTTPRequestHandler):
 
 if __name__ == "__main__":
     start_http_server(5001)
-    #server = http.server.HTTPServer(('46.101.171.233', 5000), HandleRequests)
-    server = http.server.HTTPServer(('localhost', 5000), HandleRequests)
+    server = http.server.HTTPServer(('13.40.117.12', 5000), HandleRequests)
+    #server = http.server.HTTPServer(('localhost', 5000), HandleRequests)
     server.serve_forever()
